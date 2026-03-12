12996db465d5cfbe00171767f4c33d472ade196e
mawirrr <75967459+mawirrr@users.noreply.github.com>
2026-03-12 16:33:17 +0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,3 @@
 variable "linode_token" {
-  description = "Linode API token"
-}
-
-variable "vm_name" {
-  description = "VM name"
-}
-
-variable "region" {
-  description = "Deployment region"
-}
-
-variable "plan" {
-  description = "Linode instance type"
-}
-
-variable "root_password" {
-  sensitive = true
+  type = string
 }
