93ac038b5fd9dbd679e0375163b3b4b791515e63
mawirrr <75967459+mawirrr@users.noreply.github.com>
2026-03-12 15:20:19 +0700

    Create backend.tf

---
+++ terraform/backend.tf
@@ -0,0 +1,17 @@
+terraform {
+
+  backend "s3" {
+
+    bucket = "terraform-state"
+    key    = "linode/terraform.tfstate"
+    region = "us-east-1"
+
+    endpoint = "https://ap-southeast-1.linodeobjects.com"
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+
+  }
+
+}
