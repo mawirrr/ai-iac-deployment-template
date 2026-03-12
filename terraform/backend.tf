1a989e4663df5a743b3f85579527d670ce2115dd
mawirrr <75967459+mawirrr@users.noreply.github.com>
2026-03-12 16:14:07 +0700

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -6,13 +6,16 @@ terraform {
     key    = "linode/terraform.tfstate"
     region = "us-east-1"
 
-    endpoint = "https://sg-sin-1.linodeobjects.com"
+    endpoints = {
+      s3 = "https://sg-sin-1.linodeobjects.com"
+    }
 
-    force_path_style = true
-
+    skip_requesting_account_id  = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
+
+    force_path_style = true
 
   }
 
